fee5ec42dbed78636d81c4a7f1ed87898b879653
Perceu Bertoletti <perceubertoletti@gmail.com>
2021-12-31 15:20:42 -0300

    criado um algoritimo de degrade para turtle

---
+++ python/tiktok7.py
@@ -0,0 +1,35 @@
+from turtle import Screen, Turtle
+import turtle
+screen = Screen()
+screen.colormode(255)
+screen.bgcolor("black")
+tl = Turtle()
+tl.speed(0)
+tl.hideturtle()
+
+def get_color(spectre, ini, fim):
+    if ini[spectre] > fim[spectre]:
+        ini[spectre] -= 0x1
+    elif ini[spectre] < fim[spectre]:
+        ini[spectre] += 0x1
+    return ini[spectre]
+
+def format_color(color):
+    return f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}"
+      
+def run(x, y):
+    color_ini = [0xff, 0x00, 0x59]
+    color_fim = [0x00, 0xb7, 0xff]
+    circle_size = 3
+    while (color_ini != color_fim):
+        color_ini[0] = get_color(0, ini=color_ini, fim=color_fim)
+        color_ini[1] = get_color(1, ini=color_ini, fim=color_fim)
+        color_ini[2] = get_color(2, ini=color_ini, fim=color_fim)
+        circle_size += 1
+        tl.pencolor(format_color(color_ini))
+        tl.circle(circle_size)
+        tl.right(5)
+    screen.exitonclick()
+
+screen.onclick(run)
+screen.mainloop()
