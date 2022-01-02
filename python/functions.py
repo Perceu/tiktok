a7ac3cc7128441b83c576a2fb7258147660163bd
Perceu Bertoletti <perceubertoletti@gmail.com>
2022-01-02 17:52:27 -0300

    Criado arquivos dos novos videos e feito ajustes!

--- python/functions.py
+++ python/functions.py
@@ -1,6 +1,40 @@
+from turtle import Screen
 
+def setupScreen(width, height):
+    screen = Screen()
+    screen.setup(width, height)
+    screen.colormode(255)
+    screen.bgcolor("black")
+    return screen    
 
 def square(turtle, size):
     for i in range(4):
         turtle.fd(size)
         turtle.right(90)
+
+def triangle(turtle, size):
+    for i in range(3):
+        turtle.fd(size)
+        turtle.right(120)
+
+def triangle_2(turtle, size):
+    for i in range(4):
+        turtle.fd(size)
+        turtle.right(120)
+
+def get_color(spectre, ini, fim):
+    if ini[spectre] > fim[spectre]:
+        ini[spectre] -= 0x1
+    elif ini[spectre] < fim[spectre]:
+        ini[spectre] += 0x1
+    return ini[spectre]
+
+def gradient_color(color_ini, color_fim):
+    color = [0,0,0]
+    color[0] = get_color(0, ini=color_ini, fim=color_fim)
+    color[1] = get_color(1, ini=color_ini, fim=color_fim)
+    color[2] = get_color(2, ini=color_ini, fim=color_fim)
+    return color
+
+def format_color(color):
+    return f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}"
---
+++ python/tiktok10.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+from functions import setupScreen, gradient_color, format_color, triangle_2
+screen  = setupScreen(360,640)
+tl = Turtle()
+tl.speed(0)
+tl.hideturtle()
+
+def run():
+    color_ini = [0x1a, 0x2a, 0x6c]
+    color_fim = [0xfb, 0xbb, 0x2d]
+    size = 15
+    for i in range(180):
+        color_ini = gradient_color(color_ini, color_fim)       
+        tl.pencolor(format_color(color_ini))
+        size += 5
+        if size <= 0:
+            break
+        triangle_2(tl, size)
+        tl.right(3)
+    screen.exitonclick()
+
+screen.onkey(run, "r")
+screen.listen()
+
+screen.mainloop()
