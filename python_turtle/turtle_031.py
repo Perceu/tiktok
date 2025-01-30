ac7602ec462f0ff00fc55a288791e9b6ebb2ba34
Perceu Bertoletti <perceu.bertoletti@jusbrasil.com.br>
2025-01-30 15:38:00 -0300

    add turtle animation

---
+++ python_turtle/turtle_031.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+from functions import setupScreen
+from faker import Faker
+
+screen = setupScreen(580, 1280)
+tl = Turtle()
+tl.speed(10)
+tl.hideturtle()
+fake = Faker()
+Faker.seed(0)
+
+
+def run():
+    tl.pencolor("white")
+
+    for i in range(1, 26):
+        tl.pendown()
+        tl.circle(40, 180)
+        tl.circle(-40, 180)
+        tl.circle(40, 180)
+        tl.circle(-40, 180)
+        tl.penup()
+        tl.goto(0, 0)
+        tl.setheading(15 * i)
+
+    tl.penup()
+    tl.goto(0, 0)
+    tl.setheading(90)
+    tl.pendown()
+
+    for i in range(1, 26):
+        tl.pendown()
+        tl.circle(-40, 180)
+        tl.circle(40, 180)
+        tl.circle(-40, 180)
+        tl.circle(40, 180)
+        tl.penup()
+        tl.goto(0, 0)
+        tl.setheading(15 * i)
+
+    screen.exitonclick()
+
+
+screen.onkey(run, "r")
+screen.listen()
+screen.mainloop()
